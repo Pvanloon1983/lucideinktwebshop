929b0b9ad975cf461eb4c1abace2bda5f28a9f02
Bilal van Loon <vanloon_1983@hotmail.com>
2025-07-28 00:41:23 +0200

    Change link logo

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,22 +6,13 @@
     <title>Laravel</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-  {{-- <style>
-    body {
-    background-image: url('/images/arka.jpg');
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center;
-    }
-  </style> --}}
 </head>
 <body >
 
 <header class="header">
     <div class="container">
       <div class="logo-container">
-        <img src="{{ url('/images/lucide-Inkt-logo.png') }}" alt="">
+        <a href="{{ route('home') }}"><img src="{{ url('/images/lucide-Inkt-logo.png') }}" alt=""></a>
       </div>
       <div class="desktop-navbar-container">
         <nav class="navbar">
