ed2a30c8c1de59f2a7a6841b75e5b6fd71e62f57
Bilal van Loon <vanloon_1983@hotmail.com>
2025-08-16 00:34:20 +0200

    Changed image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
 <main class="page">
         <!-- Hero: full width, controlled height -->
-        <div class="hero-image" style="width:100%; height:100vh;">
+        <div class="hero-image" style="width:100%;">
                 <img src="{{ asset('images/hero_image.png') }}" alt="Hero Image" style="width:100%; height:100%; object-fit:cover; display:block;">
         </div>
     
