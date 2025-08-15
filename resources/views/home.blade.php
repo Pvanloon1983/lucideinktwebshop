9e4a149af6a9d80671ed8811ee7bf113a3f413cc
Bilal van Loon <vanloon_1983@hotmail.com>
2025-08-16 00:06:06 +0200

    Added hero banner

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,11 @@
 <x-layout>
-<main class="container page">
-    <h2>Home</h2>
-</main>
+<main class="page">
+        <!-- Hero: full width, controlled height -->
+        <div class="hero-image" style="width:100%; height:100vh;">
+                <img src="{{ asset('images/hero_image.png') }}" alt="Hero Image" style="width:100%; height:100%; object-fit:cover; display:block;">
+        </div>
+    
+
+    
+    </main>
 </x-layout>
