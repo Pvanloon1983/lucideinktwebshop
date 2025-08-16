6af45dc0cdc12b7095f8b5cede7f71282b9d7620
Bilal van Loon <vanloon_1983@hotmail.com>
2025-08-16 17:17:09 +0200

    Added new hero section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,34 +1,18 @@
 <x-layout>
         <main class="page">
                 <!-- Hero -->
-                <div class="hero-desktop">
-                        <section class="hero hero-bg" style="background-image: url('{{ asset('images/hero_image_new.webp') }}')">
-                                <div class="hero__overlay"></div>
+                <section class="hero hero-nursi">
+                        <div class="hero__overlay" style="background-image: url('{{ asset('images/ustadh.png') }}');"></div>
 
-                                <div class="hero__content">
-                                        <p>
-                                                <strong>Weet</strong> met absolute zekerheid <br> dat het allerhoogste doel van <br> de <strong>schepping</strong> en het meest <br> verheven resultaat van onze <br> <strong>aard</strong> gegrondvest zijn in het <br><strong>geloof</strong> in <strong>ALLAH</strong>
-                                        </p>
-
-                                        <button class="btn">Lees meer</button>
-                                </div>
-                        </section>
-                </div>
-
-                <!-- Hero -->
-                <div class="hero-mobile">
-                        <section class="hero hero-bg" style="background-image: url('{{ asset('images/hero_image_new.webp') }}')">
-                                <div class="hero__overlay"></div>
-
-                                <div class="hero__content">
-                                        <p>
-                                                <strong>Weet</strong> met absolute zekerheid dat het allerhoogste doel van de <strong>schepping</strong> en het meest verheven resultaat van onze <strong>aard</strong> gegrondvest zijn in het <strong>geloof</strong> in <strong>ALLAH</strong>
-                                        </p>
-
-                                        <button class="btn">Lees meer</button>
-                                </div>
-                        </section>
-                </div>
+                        <div class="hero__content">
+                                <span class="hero__eyebrow">Risale-i Nur</span>
+                                <div class="hero__divider"></div>
+                                <p>
+                                        <strong>Weet</strong> met absolute zekerheid
+                                        dat het allerhoogste doel van de <strong>schepping</strong> en het meest verheven resultaat van onze <strong>aard</strong> gegrondvest zijn in het <strong>geloof</strong> in <strong>ALLAH</strong>
+                                </p>
+                              </div>
+                </section>
 
         </main>
 </x-layout>
